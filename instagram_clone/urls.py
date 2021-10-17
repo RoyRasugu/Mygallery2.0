b9ec1bbb0b80186fb5768dedebe90c57be576578
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-10-17 23:53:36 +0300

    add the profile and profile favorites urls

--- instagram_clone/urls.py
+++ instagram_clone/urls.py
@@ -15,12 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import Settings, settings
+from django.conf import settings
 from django.conf.urls.static import static
+
+from authy.views import UserProfile
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('post/', include('post.urls')),
     path('user/', include('authy.urls')),
+    path('<username>/', UserProfile, name='profile'),
+    path('<username>/saved', UserProfile, name='profilefavorites'),
+
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
