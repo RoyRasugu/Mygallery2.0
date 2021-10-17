99089f438305bea7b0ceccf57216fcd0abc8df60
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-10-18 00:22:06 +0300

    add the follow function

--- authy/views.py
+++ authy/views.py
@@ -1,15 +1,61 @@
 
+from django import template
+from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect, get_object_or_404
+from django.template import loader
+from django.urls.base import reverse
 from authy.forms import SignupForm, ChangePasswordForm, EditProfileForm
 from django.contrib.auth.models import User
 
+from post.models import Follow, Post, Stream
+
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import update_session_auth_hash
+from django.urls import resolve
+from django.core.paginator import Paginator
+
+from django.db import transaction
 
 from authy.models import Profile
 
-
 # Create your views here.
+def UserProfile(request,username):
+	user = get_object_or_404(User, username=username)
+	profile = Profile.objects.get(user=user)
+	url_name = resolve(request.path).url_name
+
+	if url_name == 'profile':
+		posts = Post.objects.filter(user=user).order_by('-posted')
+	else:
+		posts = profile.favorites.all()
+
+	#Profile info stats
+	posts_count = Post.objects.filter(user=user).count()
+	following_count = Follow.objects.filter(follower=user).count()
+	followers_count = Follow.objects.filter(following=user).count()
+
+	#Check follow status
+	follow_status = Follow.objects.filter(following=user, follower=request.user).exists()
+
+	#Pagination
+	paginator = Paginator(posts, 8)
+	page_number = request.GET.get('page')
+	posts_paginator = paginator.get_page(page_number)
+
+	template = loader.get_template('profile.html')
+
+	context = {
+		'posts': posts_paginator,
+		'profile': profile,
+		'url_name': url_name,
+		'posts_count': posts_count,
+		'following_count': following_count,
+		'followers_count': followers_count,
+		'follow_status': follow_status,
+	}
+
+	return HttpResponse(template.render(context, request))
+
 def Signup(request):
 	if request.method == 'POST':
 		form = SignupForm(request.POST)
@@ -79,4 +125,24 @@ def EditProfile(request):
 
 	return render(request, 'edit_profile.html', context)
 
+@login_required
+def follow(request, username, option):
+	following = get_object_or_404(User, username=username)
 
+	try:
+		f, created = Follow.objects.get_or_create(follower=request.user, following=following)
+
+		if int(option) == 0:
+			f.delete()
+			Stream.objects.filter(following=following, user=request.user).all().delete()
+		else:
+			 posts = Post.objects.all().filter(user=following)[:25]
+
+			 with transaction.atomic():
+			 	for post in posts:
+			 		stream = Stream(post=post, user=request.user, date=post.posted, following=following)
+			 		stream.save()
+
+		return HttpResponseRedirect(reverse('profile', args=[username]))
+	except User.DoesNotExist:
+		return HttpResponseRedirect(reverse('profile', args=[username]))
