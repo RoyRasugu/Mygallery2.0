e2fe2a687065fb4390c3af15d5d61e3fa209bdff
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-10-17 11:51:43 +0300

    register the models

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from post.models import Image, Stream,Tag,Follow
 
 # Register your models here.
+admin.site.register(Image)
+admin.site.register(Tag)
+admin.site.register(Follow)
+admin.site.register(Stream)
+
