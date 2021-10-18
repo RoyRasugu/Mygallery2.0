ebcd5d160e1c2f1c238beb0e612db46f6e2ac6b2
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-10-18 22:35:15 +0300

    add the methods for direct messaging

--- direct/views.py
+++ direct/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http.response import HttpResponseBadRequest
+from django.shortcuts import redirect, render
 from django.template import loader
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
 from direct.models import Message
+
+from django.db.models import Q
+from django.core.paginator import Paginator
 
 # Create your views here.
 @login_required
@@ -32,3 +37,72 @@ def inbox(request):
     template = loader.get_template('direct/direct.html')
 
     return HttpResponse(template.render(context, request))
+
+@login_required
+def Directs(request, username):
+    user = request.user
+    messages = Message.get_messages(user=user)
+    active_direct = username
+    directs = Message.objects.filter(user=user, recipient__username=username)
+    directs.update(is_read=True)
+
+    for message in messages:
+        if message['user'].username == username:
+            message['unread'] = 0
+
+    context = {
+        'directs': directs,
+        'messages': messages,
+        'active_direct': active_direct,
+    }
+    
+    template = loader.get_template('direct/direct.html')
+
+    return HttpResponse(template.render(context, request))
+
+@login_required
+def SendDirect(request):
+    from_user = request.user
+    to_user_username = request.POST.get('to_user')
+    body = request.POST.get('body')
+
+    if request.method == 'POST':
+        to_user = User.objects.get(username=to_user_username)
+        Message.send_message(from_user, to_user,body)
+        return redirect('inbox')
+    else:
+        HttpResponseBadRequest()
+
+@login_required
+def UserSearch(request):
+	query = request.GET.get('q')
+	context = {}
+	
+	if query:
+		users = User.objects.filter(Q(username__icontains=query))
+
+		#Pagination
+		paginator = Paginator(users, 6)
+		page_number = request.GET.get('page')
+		users_paginator = paginator.get_page(page_number)
+
+		context = {
+				'users': users_paginator,
+			}
+	
+	template = loader.get_template('direct/search_user.html')
+	
+	return HttpResponse(template.render(context, request))
+
+@login_required
+def NewConversation(request, username):
+    from_user = request.user
+    body = 'Says Hello!'
+
+    try:
+        to_user = User.objects.get(username=username)
+    except Exception as e:
+        return redirect('usersearch')
+    if from_user != to_user:
+        Message.send_message(from_user, to_user, body)
+    return redirect('inbox')
