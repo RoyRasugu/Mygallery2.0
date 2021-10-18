ff631243982eaeead208841662430948fa58497f
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-10-18 22:34:29 +0300

    add the search and direct message urls

--- direct/urls.py
+++ direct/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from direct.views import inbox
+from direct.views import inbox, UserSearch, Directs, SendDirect, NewConversation
 
 urlpatterns = [
     path('', inbox, name='inbox'),
+    path('new/', UserSearch, name='usersearch'),
+    path('directs/<username>', Directs, name='directs'),
+    path('send/', SendDirect, name='send_direct'),
+    path('new/<username>', NewConversation , name='newconversation'),
 ]
