ab37a04f58120f2f73d66f8cee47cde4e47b00cc
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-10-19 07:34:41 +0300

    create the notification class model

--- notifications/models.py
+++ notifications/models.py
@@ -9,6 +9,6 @@ class Notification(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="noti_from_user")
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="noti_to_user")
     notification_type = models.IntegerField(choices=NOTIFICATION_TYPES)
-    test_preview = models.CharField(max_length=90, blank=True)
+    text_preview = models.CharField(max_length=90, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     is_seen = models.BooleanField(default=False)
