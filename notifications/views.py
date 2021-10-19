a7be6e2c6906c45eed0a50c8b39f94cc052a9323
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-10-19 11:51:22 +0300

    create the countNotifications view method

--- notifications/views.py
+++ notifications/views.py
@@ -1,5 +1,4 @@
-from django import template
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.template import loader
 from django.http import HttpResponse
 
@@ -9,6 +8,7 @@ from notifications.models import Notification
 def ShowNotifications(request):
     user = request.user
     notifications = Notification.objects.filter(user=user).order_by('-date')
+    Notification.objects.filter(user=user, is_seen=False).update(is_seen=True)
 
     template = loader.get_template('notifications.html')
 
@@ -17,3 +17,15 @@ def ShowNotifications(request):
     }
 
     return HttpResponse(template.render(context, request))
+
+def DeleteNotification(request, noti_id):
+    user = request.user
+    Notification.objects.filter(id=noti_id, user=user).delete()
+    return redirect('show-notifications')
+
+def CountNotifications(request):
+    count_notifications = 0
+    if request.user.is_authenticated:
+        count_notifications = Notification.objects.filter(user=request.user, is_seen=False).count()
+
+    return {'count_notifications': count_notifications}
