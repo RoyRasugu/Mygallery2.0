6d80f74eb0b6e814ed5c49fb26ffd80d09a88350
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-10-19 07:36:12 +0300

    create the shownotification view function

--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,19 @@
+from django import template
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
 
+from notifications.models import Notification
 # Create your views here.
+
+def ShowNotifications(request):
+    user = request.user
+    notifications = Notification.objects.filter(user=user).order_by('-date')
+
+    template = loader.get_template('notifications.html')
+
+    context = {
+        'notifications': notifications,
+    }
+
+    return HttpResponse(template.render(context, request))
