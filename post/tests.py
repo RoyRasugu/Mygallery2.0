2059a320a9f91a9294c10ef9b2ce5d130f75ca9b
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-10-24 07:55:06 +0300

    add test for posts

--- post/tests.py
+++ post/tests.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from authy.models import Profile
+from post.models import Post
 
 # Create your tests here.
+class TestPost(TestCase):
+    def setUp(self):
+        self.profile_test = Profile(name='roy')
+        self.profile_test.save()
+
+        self.image_test = Post(image='default.png', name='test', caption='default test', user=self.profile_test)
+
+    def test_insatance(self):
+        self.assertTrue(isinstance(self.image_test, Post))
+
+    def test_save_image(self):
+        self.image_test.save_image()
+        images = Post.objects.all()
+        self.assertTrue(len(images) > 0)
+
+    def test_delete_image(self):
+        self.image_test.delete_image()
+        after = Profile.objects.all()
+        self.assertTrue(len(after) < 1)
