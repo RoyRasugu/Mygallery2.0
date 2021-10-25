ad73781782f1be29b390d52e5ec3b7222b14065f
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-10-25 12:19:35 +0300

    change models

--- authy/models.py
+++ authy/models.py
@@ -11,15 +11,15 @@ import os
 
 # Create your models here.
 
-def user_directory_path(instance, filename):
-    # this file will be uploaded to MEDIA_ROOT /user(id)/filename
-	profile_pic_name = 'user_{0}/profile.jpg'.format(instance.user.id)
-	full_path = os.path.join(settings.MEDIA_ROOT, profile_pic_name)
+# def user_directory_path(instance, filename):
+#     # this file will be uploaded to MEDIA_ROOT /user(id)/filename
+# 	profile_pic_name = 'user_{0}/profile.jpg'.format(instance.user.id)
+# 	full_path = os.path.join(settings.MEDIA_ROOT, profile_pic_name)
 
-	if os.path.exists(full_path):
-		os.remove(full_path)
+# 	if os.path.exists(full_path):
+# 		os.remove(full_path)
 
-	return profile_pic_name
+# 	return profile_pic_name
 
 class Profile(models.Model):
 	'''
